364e82a81bd3acfe65d1dd83a672cd74db2fdb0c
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-02-19 14:49:24 +0530

    blank tab

---
+++ lib/pages/tabs/surah_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TabSurah extends StatelessWidget {
+  const TabSurah({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
