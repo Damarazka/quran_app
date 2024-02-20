fa0f9801a38db4070cca5fb21b3d5ebb8f56d144
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-02-20 11:10:35 +0530

    quran repo

--- lib/repository/quran_repo.dart
+++ lib/repository/quran_repo.dart
@@ -1,9 +1,23 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
+import 'package:quran_app/model/ayat_model.dart';
 import 'package:quran_app/model/surah_model.dart';
 
 class QuranRepository{
   Future<List<Surah>> getListSurah() async{
     String data = await rootBundle.loadString('assets/data/list-surah.json');
     return surahFromJson(data);
+  }
+
+  Future<AyahModel> getListAyah(String id) async{
+    var response = await Dio().get("https://equran.id/api/surat/$id");
+
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.data);
+      return AyahModel.fromJson(body);
+    }
+    throw Exception("error");
   }
 }
