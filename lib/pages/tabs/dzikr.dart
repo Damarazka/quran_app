ee9da6d239f52105087d7f828b13b8baffb6b722
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-02-24 14:35:03 +0530

    dzkr tab

--- lib/pages/tabs/dzikr.dart
+++ lib/pages/tabs/dzikr.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TabDzikr extends StatelessWidget {
   const TabDzikr({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.center,
+          height: 64,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Color(0xffdf98fa), Color(0xff9055ff)])),
+          child: Text(
+            "Morning Dzikr",
+            style:
+                GoogleFonts.poppins(fontWeight: FontWeight.w500, fontSize: 16),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.center,
+          height: 64,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Color(0xffdf98fa), Color(0xff9055ff)])),
+          child: Text("Evening Dzikr",
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w500, fontSize: 16)),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.center,
+          height: 64,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Color(0xffdf98fa), Color(0xff9055ff)])),
+          child: Text("Dzikr After Shalat",
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w500, fontSize: 16)),
+        )
+      ],
+    );
   }
 }
